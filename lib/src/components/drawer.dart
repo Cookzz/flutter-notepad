4d5ebe23d1de988a73de6e17a02268900da4d8e5
Cookies <38864138+Cookzz@users.noreply.github.com>
2025-01-09 01:22:23 +0800

    updated drawer behavior
    Accommodated new pages and turn drawer into a stateful componentt

--- lib/src/components/drawer.dart
+++ lib/src/components/drawer.dart
@@ -1,35 +1,54 @@
 import 'package:flutter/material.dart';
 
-class DrawerBuilder extends StatelessWidget {
+import '../constants/routes.dart';
+
+class DrawerBuilder extends StatefulWidget {
   final list;
 
   //provide a default drawer items
-  DrawerBuilder({this.list = const [
-    {'type': 'header', 'label': 'Drawer'},
-    {'type': 'item', 'label': 'Item 1'},
-    {'type': 'item', 'label': 'Item 2'},
-    {'type': 'item', 'label': 'Item 3'},
-    {'type': 'item', 'label': 'Item 4'},
-    {'type': 'item', 'label': 'Item 5'}
-  ]});
-  
+  DrawerBuilder(
+      {this.list = const [
+        {'type': 'header', 'label': 'Home'},
+        {'type': 'item', 'label': 'Home', 'route': HOME},
+        {'type': 'item', 'label': 'Settings', 'route': SETTINGS},
+        {'type': 'item', 'label': 'About', 'route': ABOUT},
+        // {'type': 'item', 'label': 'Item 3'},
+        // {'type': 'item', 'label': 'Item 4'},
+        // {'type': 'item', 'label': 'Item 5'}
+      ]});
+
   @override
-  Widget build(BuildContext context){
+  State<DrawerBuilder> createState() => _DrawerState();
+}
+
+class _DrawerState extends State<DrawerBuilder> {
+  int _selectedIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    final list = widget.list;
+
     return Drawer(
       child: ListView.builder(
         padding: EdgeInsets.zero,
         itemCount: list.length,
         itemBuilder: (BuildContext ctxt, int index) {
-          if (list[index]['type'] == 'header'){
+          if (list[index]['type'] == 'header') {
             return DrawerHeader(
               child: Text(list[index]['label']),
-              decoration: BoxDecoration(
-                color: Colors.blue
-              ),
+              decoration: BoxDecoration(color: Colors.blue),
             );
           } else {
             return ListTile(
-              title: Text(list[index]['label'])
+              title: Text(list[index]['label']),
+              selectedColor: Colors.blue,
+              selected: index == _selectedIndex,
+              onTap: () => {
+                setState(() {
+                  _selectedIndex = index;
+                }),
+                Navigator.pushNamed(context, list[index]['route'])
+              },
             );
           }
         },
